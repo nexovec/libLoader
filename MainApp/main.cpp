848564172f7ed1f799527a10c57cb151279b39e2
nexovec <hraccesky@gmail.com>
2021-10-15 17:27:58 +0200

    refactored

--- MainApp/main.cpp
+++ MainApp/main.cpp
@@ -1,24 +1,39 @@
 #include <iostream>
 #include <windows.h>
+#include <stdint.h>
 #define FUNC_SIGNATURE(funcName) int funcName()
 typedef FUNC_SIGNATURE(funcType);
-FUNC_SIGNATURE(funcStub){
+FUNC_SIGNATURE(funcStub)
+{
     return 0;
 }
-// TODO: make static
-funcType *func = funcStub;
+static funcType *func = funcStub;
 int main()
 {
+    // load the dll
     std::cout << "Hello. I am the main app, the god of all the libraries" << std::endl;
     HMODULE loadedLib = LoadLibraryA("../../../SharedCode/bin/Debug/SharedCode.dll");
     if (!loadedLib)
     {
-        std::cout << "Couldn't load the library with error code " << GetLastError() << std::endl;
+        uint32_t errorCode = GetLastError();
+        if (errorCode == 126)
+        {
+            std::cout << "Couldn't load library with error code 126, are you sure you built it?" << std::endl;
+        }
+        else
+        {
+            std::cout << "Couldn't load the library with error code " << errorCode << std::endl;
+        }
+        return -1;
     }
-    else
+
+    // load and call a function from the dll
+    func = reinterpret_cast<funcType *>(GetProcAddress(loadedLib, "exampleFunc"));
+    if (!func)
     {
-        // TODO: get function
-        func = reinterpret_cast<funcType*>(GetProcAddress(loadedLib, "exampleFunc"));
-        func();
+        std::cout << "Couldn't load the dll function with error code " << GetLastError() << std::endl;
     }
+    func();
+
+    // TODO: give a callback function to the dll
 }
--- SharedCode/dllMain.cpp
+++ SharedCode/dllMain.cpp
@@ -1,9 +1,10 @@
 #include <windows.h>
 #include <iostream>
-extern "C" {
+extern "C"
+{
     __declspec(dllexport) int exampleFunc()
     {
-        std::cout << "and I am a procedure loaded from my wealthy parent, Mr. Dll" << std::endl;
+        std::cout << "I am a procedure. I was loaded from the Dll and I call it mommy." << std::endl;
         return 0;
     }
 }
@@ -12,6 +13,21 @@ BOOL WINAPI DllMain(
     _In_ DWORD fdwReason,
     _In_ LPVOID lpvReserved)
 {
-    std::cout << "I am the dll and I am more loaded than the Kenyan president." << std::endl;
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        std::cout << "I am the dll and I am more loaded than the Kenyan president." << std::endl;
+        break;
+
+    case DLL_THREAD_ATTACH:
+        break;
+
+    case DLL_THREAD_DETACH:
+        break;
+
+    case DLL_PROCESS_DETACH:
+        std::cout << "I am the dll and I am being unloaded, send help." << std::endl;
+        break;
+    }
     return TRUE;
 }
