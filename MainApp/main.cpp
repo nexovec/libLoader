fb7444fa2c62918917fc54cdb0322b0d4dd6b3de
nexovec <hraccesky@gmail.com>
2021-10-17 21:38:57 +0200

    updating, caching

--- MainApp/main.cpp
+++ MainApp/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <windows.h>
 #include <stdint.h>
+#include <chrono>
+#include <thread>
 
 #define SIGNATURE_OF_FUNC(functionName) int functionName()
 typedef SIGNATURE_OF_FUNC(funcType);
@@ -38,12 +40,19 @@ SIGNATURE_OF_RUN_ALL_CALLBACKS(runAllCallbacksStub)
 }
 static runAllCallbacksType *runAllCallbacks = runAllCallbacksStub;
 
-int main()
-{
+HMODULE libModule = nullptr;
+int loadLibrary(std::string& path) {
+    // TODO: check file changes
+    if (libModule != nullptr) {
+        std::cout << "Library was already loaded" << std::endl;
+        return 0;
+    }
+    // TODO: free the library if it exists
+    // TODO: create a working copy of the library
     // load the dll
-    std::cout << "Hello. I am the main app, the god of all the libraries" << std::endl;
-    HMODULE loadedLib = LoadLibraryA("../../../SharedCode/bin/Debug/SharedCode.dll");
-    if (!loadedLib)
+
+    libModule = LoadLibraryA(path.c_str());
+    if (!libModule)
     {
         uint32_t errorCode = GetLastError();
         if (errorCode == 126)
@@ -54,39 +63,53 @@ int main()
         {
             std::cout << "Couldn't load the library with error code " << errorCode << std::endl;
         }
-        return -1;
+        return 0;
     }
 
     // load and call a function from the dll
-    exampleFunc = reinterpret_cast<funcType *>(GetProcAddress(loadedLib, "exampleFunc"));
+    exampleFunc = reinterpret_cast<funcType*>(GetProcAddress(libModule, "exampleFunc"));
     if (!exampleFunc)
     {
         std::cout << "Couldn't load the dll function "
-                  << "exampleFunc"
-                  << " with error code " << GetLastError() << std::endl;
-        return -1;
+            << "exampleFunc"
+            << " with error code " << GetLastError() << std::endl;
+        return 0;
     }
     exampleFunc();
 
     // pass a callback function to the dll
-    setCbk = reinterpret_cast<setCbkType *>(GetProcAddress(loadedLib, "setCbk"));
+    setCbk = reinterpret_cast<setCbkType*>(GetProcAddress(libModule, "setCbk"));
     if (!setCbk)
     {
         std::cout << "Couldn't give callback "
-                  << "cbk"
-                  << " to the dll with error code " << GetLastError() << std::endl;
-        return -1;
+            << "cbk"
+            << " to the dll with error code " << GetLastError() << std::endl;
+        return 0;
     }
     setCbk(callbackFunction);
 
     // run all callbacks in the dll
-    runAllCallbacks = reinterpret_cast<runAllCallbacksType *>(GetProcAddress(loadedLib, "runAllCallbacks"));
+    runAllCallbacks = reinterpret_cast<runAllCallbacksType*>(GetProcAddress(libModule, "runAllCallbacks"));
     if (!runAllCallbacks)
     {
         std::cout << "Couldn't load the dll function "
-                  << "runAllCallbacks"
-                  << " with error code " << GetLastError() << std::endl;
-        return -1;
+            << "runAllCallbacks"
+            << " with error code " << GetLastError() << std::endl;
+        return 0;
     }
     runAllCallbacks();
+
+    // TODO: ensure a temp folder exists
+    return 1;
+}
+int main()
+{
+    std::string pathToLibrary = std::string("../../../SharedCode/bin/Debug/SharedCode.dll");
+    std::cout << "Hello. I am the main app, the god of all the libraries" << std::endl;
+    loadLibrary(pathToLibrary);
+    for (;;){
+        std::cout << "Tick!" << std::endl;
+        loadLibrary(pathToLibrary);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
 }
